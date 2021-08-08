92af3bde62ce8d35cc4c7c97eb45376c6dcf28e6
Amir Mustafa <amirmustafa@Amirs-MacBook-Air.local>
2021-08-08 14:53:07 +0530

    feat: adding business logic for routes - controllers and models for array type

---
+++ dist/controllers/todos.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTodos = void 0;
+var todos = [];
+const createTodos = (req, res, next) => {
+};
+exports.createTodos = createTodos;
---
+++ src/controllers/todos.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express';
+import { Todo } from '../models/todos';
+
+var todos: Todo[] = [];
+export const createTodos:RequestHandler = (req, res, next) => {
+    
+};
